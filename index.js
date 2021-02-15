f9a03d49f4ce1c8d8732e5368b3c8efdc953b8c0
Yudo Kartiko <yudobinus@gmail.com>
2021-02-15 18:29:07 +0700

    add delete feature

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const Comment = require('./Comment');
 const cors = require('cors');
 
 const app = express();
+const comments = express.Router();
+
 app.use(bodyParser.json())
+app.use(express.json())
 app.use(cors())
+app.use('/comments', comments)
 
 mongoose.connect('mongodb://localhost/comment', {
   useNewUrlParser: true,
@@ -19,13 +23,21 @@ mongoose.connect('mongodb://localhost/comment', {
   })
   .catch(err => console.log(err));
 
-app.post('/comments', (req, res) => {
+comments.post('/', (req, res) => {
   const newComment = Comment( req.body );
   newComment.save().then(() => res.json('successfully save new comment'))
 })
 
-app.get('/comments', (req, res) => {
+comments.get('/', (req, res) => {
   Comment.find().then(result => res.send(result))
+})
+
+comments.get('/', (req, res) => {
+  Comment.find().then(result => res.send(result))
+})
+
+comments.delete('/:id', (req, res) => {
+  Comment.deleteOne({ _id: req.params.id }).then(result => res.send(result));
 })
 
 app.listen(3001, () => console.log('Listening in PORT 3001'));
