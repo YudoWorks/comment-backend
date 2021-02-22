7e6a7749004b80a590d2b3671fe3283fcc119783
Yudo Kartiko <yudobinus@gmail.com>
2021-02-22 17:37:05 +0700

    Add socket.io when added new comment

--- index.js
+++ index.js
@@ -3,14 +3,23 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const Comment = require('./Comment');
 const cors = require('cors');
+const http = require('http');
+const socket = require('socket.io');
 
 const app = express();
+const server = http.Server(app);
 const comments = express.Router();
+const options={
+  cors:true,
+  origins:["http://127.0.0.1:3000"],
+ }
 
 app.use(bodyParser.json())
 app.use(express.json())
 app.use(cors())
 app.use('/comments', comments)
+
+const io = socket(server, options);
 
 mongoose.connect('mongodb://localhost/comment', {
   useNewUrlParser: true,
@@ -19,9 +28,22 @@ mongoose.connect('mongodb://localhost/comment', {
   useCreateIndex: true
   })
   .then( () => {
-  console.log('sucessfully'); 
+  console.log('sucessfully connect to database'); 
   })
   .catch(err => console.log(err));
+
+io.on('connection', socket => {
+  console.log('new connection');
+  console.log(socket.id);
+  
+  socket.on('create-new-comment', async data => {
+    const newComment = Comment(data);
+    newComment.save()
+
+    const comments = await Comment.find();
+    io.emit('display-latest-comment', comments);    
+  })
+})
 
 comments.post('/', (req, res) => {
   const newComment = Comment( req.body );
@@ -40,4 +62,4 @@ comments.delete('/:id', (req, res) => {
   Comment.deleteOne({ _id: req.params.id }).then(result => res.send(result));
 })
 
-app.listen(3001, () => console.log('Listening in PORT 3001'));
+server.listen(3001, () => console.log('Listening in PORT 3001'));
