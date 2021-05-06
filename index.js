89abc6056d37cdbc25fa52dcc11f4bddd76aea1d
Yudo Kartiko <yudobinus@gmail.com>
2021-05-06 09:52:14 +0700

    remove bodyparser

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const Comment = require('./Comment');
 const cors = require('cors');
 const http = require('http');
